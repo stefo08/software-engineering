de083edddf6a0dde2c4f220065de84304929c2f5
ITh0rn <angelo.dalfonso@student.univaq.it>
2019-01-14 17:50:17 +0100

    Aggiunta commenti al codice

--- Prototipo/DashBoard/src/View/DashBoardController.java
+++ Prototipo/DashBoard/src/View/DashBoardController.java
@@ -32,70 +32,92 @@ public class DashBoardController implements Initializable {
     private EdificioController controllerEdificio;
     private DataController controllerData;
 
+    /*
+    Il metodo initialize inizializza i Controller (Gestore, Sensore e Edificio)
+    Inizializza la lista che servirà a riempire la Table View
+    */
+
     @Override
     public void initialize(URL location, ResourceBundle resource){
 
-        controllerGestore = new GestoreController();    //crea un controller di gestore
-        controllerSensore = new SensoreController();    //crea un controller di sensore
-        controllerEdificio = new EdificioController();  //crea un controller di edificio
-        controllerData = new DataController();          //crea un controller dei dati
-        listasensori = new ArrayList<Sensor>();         //crea una ArrayList di sensori
-        Run();                                          //lancia il metodo run
+        controllerGestore = new GestoreController();
+        controllerSensore = new SensoreController();
+        controllerEdificio = new EdificioController();
+        controllerData = new DataController();
+        listasensori = new ArrayList<Sensor>();
+        Run();
 
     }
 
+    /**
+     * Il metodo Run, passa al Controller Gestore l'User di chi ha effettuato l'accesso al sistema. I Controller recuperano, l'User, l'Edificio
+     * in suo possesso e i relativi sensori con i valori [min e max]. Viene inizializzata la Table View che servirà a mostrare i dati live al cliente
+     */
+
     private void Run(){
 
-        String idEdificio = controllerGestore.getGestoreEdificio("Angelo");      //richiama il controller interroga mongo sull'id di edificio
-        DBObject sensori = controllerEdificio.getSensoriEdificio(idEdificio);    //richiama il controller di edificio gli passa l'id dell'edificio calcolato alla scorsa linea e ritorna l'object di tutti i sensori di quell'edificio
-        System.out.println(sensori);                                             //stampa i sensori appena calcolati
-        listasensori = controllerSensore.getSensoriEdificio(sensori);            //interroga il controller e restituisce i sensori di edificio e li mette in un arraylist
-        System.out.println(listasensori);                                        //stampa la lista dei sensori dall'arraylist
+        String idEdificio = controllerGestore.getGestoreEdificio("Angelo");
+        DBObject sensori = controllerEdificio.getSensoriEdificio(idEdificio);
+        System.out.println(sensori);
+        listasensori = controllerSensore.getSensoriEdificio(sensori);
+        System.out.println(listasensori);
         ObservableList<Sensor> values = FXCollections.
-                observableArrayList();                                           //observable per javafx (suppongo per mostrare i dati)
-        IDColumn.setCellValueFactory(new PropertyValueFactory<Sensor, String>("ID")); //setta la cella ID dell'fxml con l'id corrispondente
-        NumberColumn.setCellValueFactory(new PropertyValueFactory<Sensor, Integer>("numSensore")); //setta la cella NUMERO dell'fxml con il numero corrispondente
-        ValueColumn.setCellValueFactory(new PropertyValueFactory<Sensor, Integer>("value"));  //setta la cella VALORE dell'fxml con il valore corrispondente
-        DataColumn.setCellValueFactory(new PropertyValueFactory<Sensor, String>("Time"));  //setta la cella DATA dell'fxml con la data di ricezione del dato(suppongo)
+                observableArrayList();
+        IDColumn.setCellValueFactory(new PropertyValueFactory<Sensor, String>("ID"));
+        NumberColumn.setCellValueFactory(new PropertyValueFactory<Sensor, Integer>("numSensore"));
+        ValueColumn.setCellValueFactory(new PropertyValueFactory<Sensor, Integer>("value"));
+        DataColumn.setCellValueFactory(new PropertyValueFactory<Sensor, String>("Time"));
 
-            for (Sensor s : listasensori){          /*RIEMPIE LA OBSERVABLE LIST CHIAMATA values CICLANDO SUI SENSORI PRESI PRIMA E MESSI IN listsasensori*/
+            for (Sensor s : listasensori){
                 values.add(s);
             }
 
-        Table.setItems(values);                     /*riempie la table view di javafx (id=Table) con i vari valori appena presi nel ciclo sopra*/
+        Table.setItems(values);
 
+        /**
+         * Il Metodo setRowFactory è un metodo di libreria di FX ed è necessario per modificare le proprietà gradice della TableView sotto determinate
+         * condizioni. Il metodo, ogni volta che un nuovo sensore viene aggiornato nella lista, intercetta la modifica (item) e verifica le condizioni
+         * in modo da mostrare i "colori" in base al dato che viene inviato dal sensore. (Rosso: Sensore fuori range di molto, Arancione: Fuori range limite,
+         * Verde: tutto ok
+         */
 
-        Table.setRowFactory(tv -> new TableRow<Sensor>() {          //metodo che immagino crei una nuova riga nella table
-            @Override                                               //override di updateItem
-            public void updateItem(Sensor item, boolean empty) {    //Passa l'oggetto sensore e un booleano
-                super.updateItem(item, empty);                      //richiama il metodo originario
-                if (item == null) {                                 //se l'oggetto sensore è nullo allora
-                    setStyle("-fx-background-color: #ffffff;");     //cambia colore in ROSSO su quella riga (sensore ROTTO??)
+        Table.setRowFactory(tv -> new TableRow<Sensor>() {
+            @Override
+            public void updateItem(Sensor item, boolean empty) {
+                super.updateItem(item, empty);
+                if (item == null) {
+                    setStyle("-fx-background-color: #ffffff;");
                 } else {
-                    int value = item.getValue(), max = item.getMaxRange(), min = item.getMinRange();        //prenditi il dato del sensore e i rispettivi massimali e minimali
-                    if (value > max + 3 || min < min - 3) setStyle("-fx-background-color: #9e0911;");       //se value è maggiore del massimale + 3 oppure è più piccolo di minimale - 3 allora cambia colore in ROSSO SCURO
+                    int value = item.getValue(), max = item.getMaxRange(), min = item.getMinRange();
+                    if (value > max + 3 || min < min - 3) setStyle("-fx-background-color: #9e0911;");
                     if ((value > max  && value <= max + 3 ) || (value < min && value >= min - 3))
-                        setStyle("-fx-background-color: #de8101;");                                         //se il value è compreso tra max e max+3 oppure ragionamento analogo al contrario per il minimale allora setta il colore GIALLO
-                    if (value >= min && value <= max) setStyle("-fx-background-color: #007000;");           //se il valore è compreso tra il massimale e minimale colora di VERDE
+                        setStyle("-fx-background-color: #de8101;");
+                    if (value >= min && value <= max) setStyle("-fx-background-color: #007000;");
                 }
             }
         });
 
-        Thread f = new Thread(() -> {               //CREA IL THREAD f
-            while (true) {                          //while true
-                for (Sensor s : listasensori) {     //per ogni sensore in lista sensori
-                    Sensor temp = controllerData.getLastData(s.getID());    //crea un temporaneo prenditi l'ultimo dato del sensore s
-                    for (Object items : Table.getItems()) {                 //Table.getItems si prende gli items dalla dashboard fino a quel momento
-                        Sensor temptable = (Sensor) items;                  //oggetto temporaneo di tipo sensore
-                        if (temptable.getNumSensore() == temp.getNumSensore()){     //se il sensore è lo stesso
-                            temptable.setValue(temp.getValue());                    //allora setta il valore nuovo del dato ricevuto (quello nuovo credo)
-                            temptable.setTime(temp.getTime());                      //stessa solfa ma con la data nuova
+        /**
+         * Il thread è necessario per permettere la renderizzazione dei valori nella TableView in quanto l'aggiornamento sarebbe troppo veloce e non
+         * darebbe tempo a FX di listare i valori. Il thread attraverso un While(true) è in continua richiesta di nuovi valori dal server, Viene
+         * effettuato un controllo per ogni sensore presente, se il dato che arriva aggiorna un sensore, solleva il metodo sopra setRowFactory.
+         */
+
+        Thread f = new Thread(() -> {
+            while (true) {
+                for (Sensor s : listasensori) {
+                    Sensor temp = controllerData.getLastData(s.getID());
+                    for (Object items : Table.getItems()) {
+                        Sensor temptable = (Sensor) items;
+                        if (temptable.getNumSensore() == temp.getNumSensore()){
+                            temptable.setValue(temp.getValue());
+                            temptable.setTime(temp.getTime());
                             items = (Object) temp;
                         }
-                        Table.refresh();                                            //refresha la tableview
+                        Table.refresh();
                     }
                 }
-                try {                                                               //il thread va in sleep per 10 millisecondi( detto da Di Matteo che non capisce un cazzo )
+                try {
                     Thread.sleep(10);
                 } catch (InterruptedException e) {
                     e.printStackTrace();
